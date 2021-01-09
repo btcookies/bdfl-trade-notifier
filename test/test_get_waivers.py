3caa8802a6453ce13f5c2269b4242cfacf0ee5cb
brookstaylor <brooks.o.taylor@vanderbilt.edu>
2021-01-09 02:40:07 -0500

    use os to set region as env var

--- test/test_get_waivers.py
+++ test/test_get_waivers.py
@@ -6,19 +6,20 @@ from get_waivers import *
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
 
 
 @pytest.fixture
 def setup_sqs():
     with mock_sqs():
-        sqs_client = boto3.client('sqs', region_name='us-east-1')
+        sqs_client = boto3.client('sqs')
         sqs_client.create_queue(QueueName='BDFLMessageQueue')
         yield sqs_client
 
 @pytest.fixture
 def setup_dynamodb():
     with mock_dynamodb2():
-        dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
+        dynamodb_client = boto3.client('dynamodb')
         dynamodb_client.create_table(
             TableName='waivers',
             AttributeDefinitions=[
@@ -63,7 +64,7 @@ def setup_dynamodb():
             ]
         )
 
-        dynamodb_resource = boto3.resource('dynamodb', region_name='us-east-1')
+        dynamodb_resource = boto3.resource('dynamodb')
 
         waivers_table = dynamodb_resource.Table('waivers')
         waivers_table.update_item(
